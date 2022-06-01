94e00e98877a4885dde452ca0b22bd41c123d6b9
player01osu <player01osu@gmail.com>
2022-06-01 14:32:48 -0400

    bufferline no x decoration

--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -1,2 +1,22 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup {
+  options = {
+    separator_style = {"", ""},
+    indicator_icon = "",
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = false
+  }
+}
+
+require("transparent").setup({
+  enable = true,
+  extra_groups = {
+    -- akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+})
