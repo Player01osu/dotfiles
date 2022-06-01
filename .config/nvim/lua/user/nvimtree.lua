272d7b531f01a3b6d755f8bdc602483f10dfb9ba
player01osu <player01osu@gmail.com>
2022-06-01 14:36:38 -0400

    nvimtree hijack cursor, close on only buffer

--- .config/nvim/lua/user/nvimtree.lua
+++ .config/nvim/lua/user/nvimtree.lua
@@ -1,7 +1,7 @@
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
   disable_netrw = true,
-  hijack_cursor = false,
+  hijack_cursor = true,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
   ignore_buffer_on_setup = false,
@@ -12,7 +12,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   update_cwd = false,
   reload_on_bufenter = false,
   view = {
-    width = 23,
+    width = 24,
     height = 30,
     hide_root_folder = false,
     side = "left",
@@ -116,3 +116,16 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     },
   },
 } -- END_DEFAULT_OPTS
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
+
+vim.cmd([[
+	  autocmd FileType latex,tex,md,vimwiki,wiki,markdown NvimTreeClose
+	]])
