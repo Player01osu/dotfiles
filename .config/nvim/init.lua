df3e5aa7ee7b96e347e9c51f53bf78a24fbcb97b
player01osu <player01osu@gmail.com>
2022-09-24 19:20:36 -0400

    d

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,43 @@
 require('impatient')
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
+--require "user.bufferline"
 require "user.cmp"
-require "user.lualine"
-require "user.treesitter"
-require "user.bufferline"
-require "user.lsp"
-require "user.nvimtree"
+require "user.commands"
 require "user.indent"
+require "user.keymaps"
+require "user.lsp"
+require "user.lualine"
+require "user.mason"
+require "user.options"
+require "user.plugins"
+require "user.telescope"
+require "user.treesitter"
+
+vim.cmd([[
+  if exists("g:neovide")
+    lua require "user.neovide"
+  endif
+]])
+
+vim.cmd([[
+  function! VimwikiLinkHandler(link)
+    " Use Vim to open external files with the 'vfile:' scheme.  E.g.:
+    let link = a:link
+    if link =~# '^vfile:'
+      let link = link[1:]
+    else
+      return 0
+    endif
+    let link_infos = vimwiki#base#resolve_link(link)
+    if link_infos.filename == ''
+      echomsg 'Vimwiki Error: Unable to resolve link!'
+      return 0
+    else
+      exe 'tabnew ' . fnameescape(link_infos.filename)
+      return 1
+    endif
+  endfunction
+]])
 
 --require "user.material"
 --require "user.compleet"
+--require "user.nvimtree"
--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -13,24 +13,33 @@ local keymap = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 
 -- Normal --
---keymap("n", "<C-K>", ":CocEnable<CR>", opts)
---keymap("n", "<C-J>", ":CocDisable<CR>", opts)
-keymap("n", "<C-B>", ":Goyo<CR>", opts)
-keymap("n", "<C-N>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<C-H>", ":bd<CR>", opts)
 keymap("n", "<C-L>", ":set wrap<CR>", opts)
+keymap("n", "<C-N>", ":Ex<CR>", opts)
 keymap("n", "<C-P>", ":set nowrap<CR>", opts)
 keymap("n", "<C-X>", ":noh<CR>", opts)
-keymap("n", "<C-S>", ":foldclose<CR>", opts)
-keymap("n", "<C-Q>", ":foldopen<CR>", opts)
-keymap("n", "<C-H>", ":bd<CR>", opts)
+
+keymap("n", "<leader>li", ":LspStart<CR>", opts)
+keymap("n", "<leader>lo", ":LspStop<CR>", opts)
+
+keymap("n", "<leader>con", ":e ~/.config/nvim/init.lua<CR>", opts)
+
 keymap("n", "<leader>s", ":Neoformat<CR> :w<CR>", opts)
-keymap("n", "<leader>p", ":LspStop<CR>", opts)
-keymap("n", "<leader>o", ":LspStart<CR>", opts)
-keymap("n", "<leader>y", ":%y+<CR>", opts)
-keymap("n", "<leader>a", "zza", opts)
+
+keymap("n", "<leader>ya", ":%y+<CR>", opts)
+keymap("n", "<leader>y", "\"+y", opts)
+keymap("n", "<leader>Y", "\"+Y", opts)
+
+keymap("n", "<leader>d", "\"+d", opts)
+keymap("n", "<leader>D", "\"+D", opts)
+
+keymap("n", "<leader>p", "\"+p", opts)
+keymap("n", "<leader>P", "\"+P", opts)
+
 keymap("n", "<leader>A", "zzA", opts)
-keymap("n", "<leader>i", "zzi", opts)
 keymap("n", "<leader>I", "zzI", opts)
+keymap("n", "<leader>a", "zza", opts)
+keymap("n", "<leader>i", "zzi", opts)
 keymap("n", "<leader>gg", ":LazyGit<CR>", opts)
 keymap("n", "<leader>H", ":set signcolumn=yes:9<CR>", opts)
 keymap("n", "<leader>J", ":set signcolumn=yes:2<CR>", opts)
@@ -38,17 +47,86 @@ keymap("n", "<leader>J", ":set signcolumn=yes:2<CR>", opts)
 keymap("n", "<leader>h", ":bp<CR>", opts)
 keymap("n", "<leader>l", ":bn<CR>", opts)
 
-keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-keymap("n", "<leader>fn", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
-keymap("n", "<leader>fo", "<cmd>lua require('telescope.builtin').oldfiles()<cr>", opts)
+vim.keymap.set(
+	'n',
+	'<leader>ff',
+	function()
+		require('telescope.builtin').find_files()
+	end,
+	opts
+)
 
---keymap("n", "gd", "<Plug>(coc-definition)", opts);
---keymap("n", "gy", "<Plug>(coc-type-definition)", opts);
---keymap("n", "gi", "<Plug>(coc-implementation)", opts);
---keymap("n", "gr", "<Plug>(coc-references)", opts);
+vim.keymap.set(
+	'n',
+	'<leader>fg',
+	function()
+		require('telescope.builtin').live_grep()
+	end,
+	opts
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>fb',
+	function()
+		require('telescope.builtin').buffers()
+	end,
+	opts
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>fn',
+	function()
+		require('telescope.builtin').help_tags()
+	end,
+	opts
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>fo',
+	function()
+		require('telescope.builtin').oldfiles()
+	end,
+	opts
+)
+
+
+
+-- Switch and delete git worktrees
+-- <c-d> - deletes that worktree
+-- <c-f> - toggles forcing of the next deletion
+vim.keymap.set(
+	'n',
+	'<leader>fs',
+	function()
+		require('telescope').extensions.git_worktree.git_worktrees()
+	end,
+	opts
+)
+
+-- Create new git worktree
+vim.keymap.set(
+	'n',
+	'<leader>fc',
+	function()
+		require('telescope').extensions.git_worktree.create_git_worktree()
+	end,
+	opts
+)
 
 -- insert mode
-
 keymap("i", "<C-P>", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", opts)
+
+keymap("i", "<C-J>", "<Esc>", opts)
+
+-- visual mode
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+keymap("v", "<leader>y", "\"+y", opts)
+
+keymap("v", "<leader>d", "\"+d", opts)
+
+keymap("v", "<leader>p", "\"+p", opts)
