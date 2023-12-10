b1a179b65966715c91760864dd650939ad3a0ff1
player01osu <player01osu@gmail.com>
2023-12-10 18:47:32 -0500

    cleanup colorscheme

--- .config/nvim/colors/colorscheme.lua
+++ .config/nvim/colors/colorscheme.lua
@@ -1,39 +1,33 @@
 -- colors/colorscheme.lua
 local colors = {
--- content here will not be touched
--- PATCH_OPEN
-Conceal = {fg = "cyan2", bg = "NONE"},
-CursorLine = {bg = "Grey21"},
-CursorLineNr = {fg = "Yellow", bg = "NONE"},
-FoldColumn = {fg = "cyan2", bg = "NONE"},
-Folded = {fg = "#F6F66A", bg = "NONE"},
-Function = {},
-Identifier = {},
-LineNr = {fg = "grey36", bg = "NONE"},
-MsgArea = {},
-NormalFloat = {bg = "NONE"},
-Operator = {fg = "NONE", bg = "NONE"},
-Pmenu = {bg = "NONE"},
-PreProc = {fg = "#FAFA66", bg = "NONE"},
-QuickFixLine = {bg = "gray31"},
-SignColumn = {},
-Special = {},
-Statement = {fg = "#F6F66A", bg = "NONE", bold = true},
-Title = {fg = "Cyan", bg = "NONE"},
-Todo = {fg = "Grey10", bg = "#F7F708"},
-Type = {fg = "#62FD62", bg = "NONE", bold = true},
-TSVariableBuiltin = {link = "Type"},
-DiagnosticUnnecessary = {fg = "NONE", bg = "NONE"},
-Visual = {bg = "Grey32"},
-Whitespace = {fg = "Grey26"},
--- highlight StatusLine guifg=#303030 guibg=#BBBBBB
-StatusLine = {fg="#CFCFCF", bg="#151515", bold = false},
-StatusLineNC = {link="StatusLine"},
-TabLine = {link="StatusLine"},
--- PATCH_CLOSE
--- content here will not be touched
+	Conceal = {fg = "cyan2", bg = "NONE"},
+	CursorLine = {bg = "Grey21"},
+	CursorLineNr = {fg = "Yellow", bg = "NONE"},
+	FoldColumn = {fg = "cyan2", bg = "NONE"},
+	Folded = {fg = "#F6F66A", bg = "NONE"},
+	Function = {},
+	Identifier = {},
+	LineNr = {fg = "grey36", bg = "NONE"},
+	MsgArea = {},
+	NormalFloat = {bg = "NONE"},
+	Operator = {fg = "NONE", bg = "NONE"},
+	Pmenu = {bg = "NONE"},
+	PreProc = {fg = "#FAFA66", bg = "NONE"},
+	QuickFixLine = {bg = "gray31"},
+	SignColumn = {},
+	Special = {},
+	Statement = {fg = "#F6F66A", bg = "NONE", bold = true},
+	Title = {fg = "Cyan", bg = "NONE"},
+	Todo = {fg = "Grey10", bg = "#F7F708"},
+	Type = {fg = "#62FD62", bg = "NONE", bold = true},
+	TSVariableBuiltin = {link = "Type"},
+	DiagnosticUnnecessary = {fg = "NONE", bg = "NONE"},
+	Visual = {bg = "Grey32"},
+	Whitespace = {fg = "Grey26"},
+	StatusLine = {fg="#CFCFCF", bg="#151515", bold = false},
+	StatusLineNC = {link="StatusLine"},
+	TabLine = {link="StatusLine"},
 }
---hi StatusLineNC guifg=NONE      guibg=NONE     guisp=NONE gui=reverse      ctermfg=NONE      ctermbg=NONE     cterm=reverse
 
 -- colorschemes generally want to do this
 vim.cmd("highlight clear")
