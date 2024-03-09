e6a36f72c16fc02f6d809400182ef7fd37362290
player01osu <player01osu@gmail.com>
2024-03-08 20:32:36 -0500

    Remove 'R' command in neovim

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -10,7 +10,7 @@ local create_user_command = vim.api.nvim_create_user_command
 
 --                 cmd name          command           opts
 create_user_command("Erc", "e ~/.config/nvim/init.lua", {})
-create_user_command("R", "w | make!", {})
+--create_user_command("R", "w | make!", {})
 create_user_command("M", "make!", {})
 create_user_command("Rv", "w | make!", {})
 create_user_command("Rh", "w | make!", {})
