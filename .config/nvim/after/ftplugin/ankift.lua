7626e8bb23eb3639219d771288cf8979a73449a3
player01osu <player01osu@gmail.com>
2024-03-08 20:31:13 -0500

    Feature to open image in ankift

--- .config/nvim/after/ftplugin/ankift.lua
+++ .config/nvim/after/ftplugin/ankift.lua
@@ -1,4 +1,16 @@
+local opts = { noremap = true, silent = true }
+
 vim.opt_local.smartindent = false
 vim.opt_local.smartcase = true
 vim.opt_local.ignorecase = true
 
+local image_path = os.getenv("XDG_DATA_HOME") .. "/Anki2/User 1/collection.media/"
+
+vim.keymap.set("n", "<leader><leader>i", function()
+	local Job = require'plenary.job'
+	local filename = vim.fn.getreg("+")
+	Job:new({
+		command = 'nsxiv',
+		args = { image_path .. "/" .. filename },
+		}):start() -- or sync()
+end, opts)
